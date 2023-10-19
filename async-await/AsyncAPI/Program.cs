e37a31b0bc678c61e9f25374c712085ec08dc9a9
Ryan Maffit <rmaffit@sancsoft.com>
2023-10-19 15:35:01 -0400

    Added Parallel.ForEachAsync example.

--- async-await/AsyncAPI/Program.cs
+++ async-await/AsyncAPI/Program.cs
@@ -4,25 +4,48 @@ var app = builder.Build();
 
 app.MapGet("/", async (ILogger<Program> logger) =>
 {
-    await AsyncIterate(logger, 100);
+    await AsyncIterate(logger, 100, false);
 });
 
 app.MapGet("/ct", async (ILogger<Program> logger, CancellationToken token) =>
 {
-    await AsyncIterate(logger, 100, token);
+    await AsyncIterate(logger, 100, false, token);
+});
+
+app.MapGet("/parallel", async (ILogger<Program> logger) =>
+{
+    await AsyncIterate(logger, 100, true);
+});
+
+app.MapGet("/parallel/ct", async (ILogger<Program> logger, CancellationToken token) =>
+{
+    await AsyncIterate(logger, 100, true, token);
 });
 
 app.Run();
 
-async Task AsyncIterate(ILogger<Program> logger, int iterations, CancellationToken token = default)
+async Task AsyncIterate(ILogger<Program> logger, int iterations, bool parallel = false, CancellationToken token = default)
 {
-    for(var i = 0; i < iterations; i++)
+    if(parallel)
     {
-        // Alternatively you can cleanly exit depending on circumstances by checking `token.IsCancellationRequested` 
-        // and breaking out of the loop. This can be handy if you need to perform some cleanup.
-        token.ThrowIfCancellationRequested();
+        await Parallel.ForEachAsync(Enumerable.Range(0, 100), new ParallelOptions() {
+            CancellationToken = token,
+            MaxDegreeOfParallelism = 10
+        }, async (i, ct) => {
+            logger.LogInformation("Processing {i}", i);
+            await Task.Delay(1000, token);
+        });
+    }
+    else
+    {
+        for(var i = 0; i < iterations; i++)
+        {
+            // Alternatively you can cleanly exit depending on circumstances by checking `token.IsCancellationRequested` 
+            // and breaking out of the loop. This can be handy if you need to perform some cleanup.
+            token.ThrowIfCancellationRequested();
 
-        logger.LogInformation("Processing {i}", i);
-        await Task.Delay(100, token);
+            logger.LogInformation("Processing {i}", i);
+            await Task.Delay(100, token);
+        }
     }
 }
