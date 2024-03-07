f643b654bcf1df5961b961fd3fd245ccd5f83d85
Ryan Maffit <rmaffit@sancsoft.com>
2024-03-07 15:46:58 -0500

    Added Serilog wotj JSON console and Loki sinks.

--- structured-logging/Worker.cs
+++ structured-logging/Worker.cs
@@ -11,13 +11,44 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        var random = new Random();
+
+        Dictionary<int, Guid> ids = new();
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
+            for (var i = 0; i < 10; i++)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                Guid id;
+                if(!ids.ContainsKey(i))
+                {
+                    ids.Add(i, Guid.NewGuid());
+                }
+
+                id = ids[i];
+                
+                using (_logger.BeginScope(new Dictionary<string, object>() { { "someRecordId", id } }))
+                {
+                    try
+                    {
+                        _logger.LogInformation("Attempting to process index {index}", i);
+
+                        if(random.Next(100) > 75)
+                        {
+                            throw new Exception("Random exception!");
+                        }
+
+                        _logger.LogInformation("Worker {index} running at: {time}", i, DateTimeOffset.Now);
+                        await Task.Delay(1000, stoppingToken);
+                    }
+                    catch(Exception ex)
+                    {
+                        _logger.LogError(ex, "Error processing index {index}", i);
+                    }
+                }
             }
-            await Task.Delay(1000, stoppingToken);
+
+            await Task.Delay(10000, stoppingToken);
         }
     }
 }
